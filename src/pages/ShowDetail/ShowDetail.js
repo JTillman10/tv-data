5bd08fbeea9fb2aeba241258ad131560cbdbd913
Justin Tillman <tillman.55@buckeyemail.osu.edu>
2019-03-04 00:20:23 -0500

    removing unneeded seasons component

--- src/pages/ShowDetail/ShowDetail.js
+++ src/pages/ShowDetail/ShowDetail.js
@@ -5,14 +5,21 @@ import { connect } from 'react-redux';
 import './ShowDetail.scss';
 
 import { Overview } from '../../components/Show/Overview/Overview';
-import Seasons from '../../containers/Seasons/Seasons';
+import { RatingsChart } from '../../components/Show/RatingsChart/RatingsChart';
+import { SeasonList } from '../../components/Show/SeasonsList/SeasonList';
 import { Episodes } from '../../components/Show/Episodes/Episodes';
 
-import { getShow, getEpisode } from '../../store/show/show.actions';
+import {
+  getShow,
+  getEpisode,
+  updateSelectedSeason,
+  getEpisodesForSeason
+} from '../../store/show/show.actions';
 
 export class ShowDetail extends Component {
   componentDidMount() {
     this.props.onGetShow(this.props.match.params.showId);
+    this.props.onGetEpisodesForSeason(this.props.match.params.showId, 1);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -25,8 +32,17 @@ export class ShowDetail extends Component {
     this.props.onGetEpisode(this.props.match.params.showId, seasonNumber, episodeNumber);
   };
 
-  seasonSelectedHandler = () => {
+  seasonSelectedHandler = event => {
     // TODO: add reset episode action
+    const newSeasonNumber = event.target.value;
+    event.preventDefault();
+
+    this.props.onUpdateSelectedSeason(newSeasonNumber);
+    this.props.onGetEpisodesForSeason(
+      this.props.match.params.showId,
+      newSeasonNumber,
+      this.props.showInfo.number_of_seasons
+    );
   };
 
   render() {
@@ -42,12 +58,20 @@ export class ShowDetail extends Component {
           </div>
           <div className="section">
             <div className="container">
-              <Seasons
-                showId={this.props.match.params.showId}
-                totalSeasons={this.props.showInfo.number_of_seasons}
-                selectEpisode={this.episodeSelectedHandler}
-                selectSeason={this.seasonSelectedHandler}
-              />
+              <div className="box">
+                <SeasonList
+                  currentSelectedSeason={this.props.selectedSeason}
+                  numberOfSeasons={this.props.showInfo.number_of_seasons}
+                  onSelectSeason={this.seasonSelectedHandler}
+                />
+                <RatingsChart
+                  showId={this.props.match.params.showId}
+                  episodes={this.props.episodes}
+                  numberOfSeasons={this.props.showInfo.number_of_seasons}
+                  selectedSeason={this.props.selectedSeason}
+                  onSelectEpisode={this.episodeSelectedHandler}
+                />
+              </div>
             </div>
           </div>
           <div className="container">
@@ -64,15 +88,20 @@ export class ShowDetail extends Component {
 const mapStateToProps = state => {
   return {
     showInfo: state.show.showInfo,
-    selectedEpisode: state.show.selectedEpisode
+    // selectedEpisode: state.show,
+    selectedSeason: state.show.selectedSeason,
+    episodes: state.show.episodes
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     onGetShow: showId => dispatch(getShow(showId)),
-    onGetEpisode: (showId, seasonNumber, episodeNumber) =>
-      dispatch(getEpisode(showId, seasonNumber, episodeNumber))
+    onGetEpisodesForSeason: (showId, newSeasonNumber, totalSeasons) =>
+      dispatch(getEpisodesForSeason(showId, newSeasonNumber, totalSeasons)),
+    onUpdateSelectedSeason: newSeasonNumber => dispatch(updateSelectedSeason(newSeasonNumber))
+    // onGetEpisode: (showId, seasonNumber, episodeNumber) =>
+    //   dispatch(getEpisode(showId, seasonNumber, episodeNumber))
   };
 };
 
