449baf28abf72488785eb0c0116a6967975401c4
Justin Tillman <tillman.55@buckeyemail.osu.edu>
2019-03-18 02:00:54 -0400

    Update ShowCard.js

--- src/components/Show/ShowCards/ShowCard/ShowCard.js
+++ src/components/Show/ShowCards/ShowCard/ShowCard.js
@@ -5,12 +5,15 @@ import Image from '../../../UI/Image/Image';
 
 export const ShowCard = props => {
   const detailLink = `/shows/${props.showId}`;
+  const image = props.imageUrl ? (
+    <Image url={props.imageUrl} alt={props.showName} />
+  ) : (
+    <div style={{ height: '328.688px' }} />
+  );
   return (
     <Link to={detailLink}>
       <div className="card grow">
-        <div className="card-image">
-          <Image url={props.imageUrl} alt={props.showName} />
-        </div>
+        <div className="card-image">{image}</div>
         <div className="card-content has-text-centered">
           <div className="content">{props.showName}</div>
         </div>
